a1bdd808f60a6703164eb908a8fe7621b88f5b80
DivinePius <azubuikedivineuche@gmail.com>
2023-09-07 14:49:12 +0100

    Added personal loan file

---
+++ personal-loans.php
@@ -0,0 +1,81 @@
+<?php $page_title = "Personal Loan"; require_once('components/header.php') ?>
+    <!-- Header end-->
+
+
+    <main>
+        <!-- BreadCrumb start -->
+        <section class="breadcrumb-area">
+            <div class="breadcrumb-widget  breadcrumb-widget-3 pt-200 pb-200"
+                style="background-image: url(img/breadcrumb/bg-3.jpg);">
+
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-7 mx-auto">
+                            <div class="breadcrumb-content pt-100">
+                                <h1>PERSONAL LOAN</h1>
+                                <ul>
+                                    <!-- <li><a href="index.html">home</a></li> -->
+                                    <li>Personal Loan</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- BreadCrumb end -->
+
+        <!-- Content start here -->
+        <section class="pt-140 pb-100 get-touch-area bg_white">
+            <div class="container">
+        
+                <!-- Start writing code from here -->
+                <h3>Listening to you</h3>
+
+                <p>"You first" means we begin by listening to your individual situation</p>
+
+                    <p class="mt-5">Perhaps you are shopping for a vehicle or another personal need. Call a lender at <a href="" class='color-blue'>info@swissapexprivateb.com</a> or come in to one of our branches. You may also complete a Jumpstart application to set-up a meeting with a lender at a branch near you.</p>
+
+                        <h3 class="mt-5">Disclosure For All Interest Bearing Deposit Accounts</h3>
+                        <p>The following applies to all interest bearing accounts listed on this schedule of fees and services. The interest rate and Annual Percentage Yield (APY) are subject to change weekly.</p>
+
+                        <ul class="feature-list">
+                            <li>- Balance Computation Method: We use the daily balance method to calculate the interest on your account. This method applies the daily periodic rate to the principal in the account on each day. Interest is paid (credited) and compounded monthly, except for Regular Savings which is paid (credited) and compounded quarterly.</li>
+
+                            <li>- Accrual: Interest begins to accrue no later than the day we receive full credit for the deposit of non-cash items (for example checks).</li>
+
+                            <li>- All interest bearing accounts are variable rate accounts. We may change the rate at our discretion.</li>
+
+                            <li>- If you close your account before interest is credited, you will receive interest only if you notify us at the time of closing. If you do not tell us you are closing your account you will not receive your accrued interest.</li>
+
+                            <li>- Effect of Fees: fees may reduce earnings.</li>
+                        </ul>
+
+
+            </div>
+        </section>
+        <!-- Content ends here -->
+
+    </main>
+
+        <!-- footer start -->
+        <?php require_once ('components/footer.php'); ?>
+        <!-- footer end -->
+
+    <!-- Back to top button -->
+    <a id="back-to-top" title="Back to Top"></a>
+
+    <!-- JS here -->
+    <script type="text/javascript" src="js/jquery-3.6.0.min.js"></script>
+    <script type="text/javascript" src="js/preloader.js"></script>
+    <script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
+    <script type="text/javascript" src="js/jquery.smoothscroll.min.js"></script>
+    <script type="text/javascript" src="js/jquery.waypoints.min.js"></script>
+    <script type="text/javascript" src="js/jquery.counterup.min.js"></script>
+    <script type="text/javascript" src="js/jquery.nice-select.min.js"></script>
+    <script type="text/javascript" src="js/wow.min.js"></script>
+    <script src="https://unpkg.com/ionicons@5.4.0/dist/ionicons.js"></script>
+    <script type="text/javascript" src="js/custom.js"></script>
+</body>
+
+</html>
