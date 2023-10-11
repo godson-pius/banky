a3ecb7248c17382a2057e3a61712e30cd25bb2d5
Godson Pius <40927597+godson-pius@users.noreply.github.com>
2023-10-11 10:37:04 +0100

    Increased logo

--- components/footer.php
+++ components/footer.php
@@ -4,7 +4,7 @@
                     <div class="row">
                         <div class="col-lg-3 col-sm-6">
                             <div class="f-widget about-widget">
-                                <a href="index" class="p-0 m-0"><img src="img/logo/Logo.png" alt="logo" width="120"></a>
+                                <a href="index" class="p-0 m-0"><img src="img/logo/Logo.png" alt="logo" width="170"></a>
                                 <p>Solutions to help you along your financial journey</p>
                             </div>
                         </div>
